920b181e0f6189aad42e3b30f2276816380f8472
highnitin <nitinjoshi983@gmail.com>
2024-01-24 06:32:01 +0530

    Revised episode 01, added learning pointers.

--- episode01/app.js
+++ episode01/app.js
@@ -1,3 +1,63 @@
+/*
+<div id="parent">
+    
+    <div id="child1">
+        <h1 id="heading1"> Heading 1 for child 1 </h1>
+        <h2> Heading 2 for child 1 </h2>
+    </div>
+    
+    <div id="child2"> <p id="paragraph1"> Paragraph for child 2 </p> </div>
+
+
+</div>
+*/
+let demoText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet quae nulla accusantium saepe neque ipsam voluptates! Earum sed fugit recusandae dicta distinctio libero id dolor nobis tempora quasi? Ipsam, minus!";
+
+let parent = React.createElement(
+    "div",
+    {id:"parent", class:"border"},
+    [
+        React.createElement(
+            "div",
+            {id:"child1"},
+            [
+                React.createElement("h1", {id:"heading1"}, "Heading 1 for child 1."),
+                React.createElement("h2", {id:"heading2"}, "Heading 2 for child 1."),
+            ]),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+                React.createElement("p", {id:"paragraph1"}, "Paragraph for child 2."),
+                React.createElement("p", {id:"paragraph2"}, demoText),
+            ]),
+    ]);
+
+    console.log("First console (before render of parent)", parent)
+    
+    let root = ReactDOM.createRoot(document.getElementById("root"));
+    console.log("First console for root before render", root)
+    root.render(parent);
+    console.log("Second console for root after render", root)
+    // console.log("Second console", root)
+    
+    console.log("Second console (after render of parent)", parent)
+
+// const para = React.createElement("p", {id: "paraDemo", class: "paraClass"}, demoText);
+// 
+
+
+// const heading = React.createElement("h2", {} ,"Hello Developer");
+// const rootID = document.getElementById("root");
+
+
+// console.log(para)
+
+// const root = ReactDOM.createRoot(rootID);
+// root.render(heading);
+// root.render(para);
+
+
 // const heading1 = React.createElement(
 //   "h1",
 //   { id: "heading1", design: "marvel" },
@@ -34,13 +94,13 @@
 </div> */
 
 
-const element = React.createElement("div", {"id":"parent"}, [React.createElement("div", {'id':"child1"}, [React.createElement("h1", {"className": "heading"}, "Hello I'm h1 from parent. I'm child1."), React.createElement("h2", {"className": "heading"}, "Hello I'm h2 from parent. I'm child2.")]), React.createElement("div", {'id':"child2"}, [React.createElement("h1", {"className": "heading"}, "Hello I'm h1 from child2. Also I'm a child1."), React.createElement("h2", {"className": "heading"}, "Hello I'm h2 from child2. Also I'm a child2.")])]
-);
+// const element = React.createElement("div", {"id":"parent"}, [React.createElement("div", {'id':"child1"}, [React.createElement("h1", {"className": "heading"}, "Hello I'm h1 from parent. I'm child1."), React.createElement("h2", {"className": "heading"}, "Hello I'm h2 from parent. I'm child2.")]), React.createElement("div", {'id':"child2"}, [React.createElement("h1", {"className": "heading"}, "Hello I'm h1 from child2. Also I'm a child1."), React.createElement("h2", {"className": "heading"}, "Hello I'm h2 from child2. Also I'm a child2.")])]
+// );
 
-console.log(element);
+// console.log(element);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
 
-root.render(element);
+// root.render(element);
 
