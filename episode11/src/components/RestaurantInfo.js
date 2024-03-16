28a260e9658da3c89c5ac7a8393b0c1214a3b2c3
highnitin <nitinjoshi983@gmail.com>
2024-03-17 03:13:26 +0530

    Added css for rating section in Restautant Info

--- episode11/src/components/RestaurantInfo.js
+++ episode11/src/components/RestaurantInfo.js
@@ -12,9 +12,9 @@ const RestaurantInfo = (props) => {
 	} = props.data;
 
 	return (
-		<section className="w-full flex border-b-2 border-pink-400 border-dotted py-5">
+		<section className="w-full flex justify-between border-b-2 border-pink-400 border-dotted py-5">
 			<section className="w-10/12 justify-between">
-				<h2 className="text-2xl mb-2 text-red-400">{name}</h2>
+				<h2 className="text-2xl font-semibold mb-2 text-red-400">{name}</h2>
 				<p className="text-gray-300">{cuisines.join(", ")}</p>
 				<p className="text-gray-300">
 					{locality ? locality : null}
@@ -23,18 +23,20 @@ const RestaurantInfo = (props) => {
 				</p>
 			</section>
 
-			<section className="w-2/12 flex flex-col items-center justify-evenly ">
+			<section className="w-auto flex flex-col items-center justify-evenly border border-gray-400 rounded-md px-2">
 				<section className="flex justify-center items-center space-x-1">
 					<img
 						src={greenStarIcon}
-						className="w-auto h-5"
+						className="w-auto h-4"
 					/>
-					<p className="text-lg font-semibold text-green-500">
+					<p className="text-md font-semibold text-green-500">
 						{avgRatingString}
 					</p>
 				</section>
-				<span className="border-b-2 border-gray-400 w-9/12"> </span>
-				<p className="text-xs text-gray-300">{totalRatingsString}</p>
+				<span className="border-b-2 border-gray-400 w-full"> </span>
+				<p className="text-[11px] font-semibold text-gray-300">
+					{totalRatingsString}
+				</p>
 			</section>
 		</section>
 	);
